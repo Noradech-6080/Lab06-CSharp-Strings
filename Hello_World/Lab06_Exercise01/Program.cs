0ef9b06cc13427f3f8aac2ad5b37b4111bbe6591
Noradech <prabnrd@gmail.com>
2025-03-22 20:04:50 +0700

    ส่งงาน

---
+++ Hello_World/Lab06_Exercise01/Program.cs
@@ -0,0 +1,4 @@
+﻿﻿System.Console.WriteLine("Hello, I'm 66030080");
+System.Console.WriteLine("Please enter your name");
+var name = System.Console.ReadLine();  
+System.Console.WriteLine($"Your name is {name}");
